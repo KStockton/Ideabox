e77adadab6e797a42064fd3d11f84a1a42055751
Niraj Aryal <niroz.aryal11@gmail.com>
2018-12-22 16:45:08 -0700

    extra functions

--- idea.js
+++ idea.js
@@ -14,6 +14,7 @@ class Idea{
 		localStorage.setItem('cards',JSON.stringify(ideasCollection));
 	}
 	deleteFromStorage(ideasCollection){
+		console.log('hey');
 		localStorage.setItem('cards',JSON.stringify(ideasCollection));
 
 	}
