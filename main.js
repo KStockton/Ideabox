ee750b7ed02357cc7d9880698ca6a6548ceb5e2c
Michael King-Stockton <kingstockton@gmail.com>
2018-12-23 10:35:29 -0700

    fixed some syntax

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-
 // Step 1 Selects title id and assigns it to variable title
-
 var title = document.querySelector('#title'); 
 // Step 2 Selects body id and assigns it to variable body
 var body = document.querySelector('#body');
@@ -10,17 +8,6 @@ var saveButton = document.querySelector('#savebtn');
 var bottomSection = document.querySelector('.bottom-section');
 // Step 11 Create ideasCollection variable to equal empty array.
 var ideasCollection = JSON.parse(localStorage.getItem('cards')) || [];
-
-// var idea = new Idea(title.value, body.value);
-
-
-
-
- 
- // console.log(idea);
-
-
-
 
 // Step 4 Add an event listener to saveButton variable (saveReturn not defined yet) 
 saveButton.addEventListener('click',saveReturn);
@@ -38,9 +25,6 @@ bottomSection.addEventListener('click',function(event){
 
   }
 });
-
-
-
 // Step 5 Creating a function for --> new idea with the provided title and body should appear in the idea list.
 // The text fields should be cleared and ready to accept a new idea.
 // The page should not reload.
@@ -62,45 +46,40 @@ function saveReturn(e){
 };
 
 // Step 9 Create function appendCard to pass an instance called idea (with its properties) and placing it inside of html assignd as card.
-function appendCard(idea){
+function appendCard(idea) {
 
-	var card = `<article class="card" id="${idea.id}">
+	var card = 
+      `<article class="card" id="${idea.id}">
         <h2 class="card-title">${idea.title}</h2>
         <p class="card-body">${idea.body} </p>
-        <div class="bottom-icons">
-          <div class="up-down-icons">
-            <img src="images/upvote.svg" class="upvote1">
-            <img src="images/downvote.svg" class="downvote1">
-            <h4 class="quality">${idea.quality}</h4>
-          </div>
+          <div class="bottom-icons">
+            <div class="up-down-icons">
+              <img src="images/upvote.svg" class="upvote1">
+              <img src="images/downvote.svg" class="downvote1">
+              <h4 class="quality">${idea.quality}</h4>
+            </div>
           <aside class="close-icon">
             <img src="images/delete.svg" class="deleteicon">
           </aside>
-          </div>
+        </div>
       </article>`;
 // Step 9a Puts card variable on top of bottom section
       	bottomSection.innerHTML = card + bottomSection.innerHTML;
        // bottomSection.insertAdjacentHTML('afterend',card);
 }
 
-
 window.onload = loaded;
 
-function loaded(){
-	if(localStorage.getItem('cards') !== null){
+function loaded() {
+	if(localStorage.getItem('cards') !== null) {
 		var parsed = JSON.parse(localStorage.getItem('cards'));
-		parsed.map(function(e){
+		parsed.map(function(e) {
 			appendCard(e);
 		})
-		
 	};
 };
 
-
-
-
-
-function deleteCard(event){
+function deleteCard(event) {
   // debugger
   var idea = new Idea(title.value, body.value);
   var element = event.target.parentElement.parentElement.parentElement;
@@ -113,17 +92,15 @@ function deleteCard(event){
   
   ideasCollection.splice(index,1);
   
-  
   element.remove();
 
   idea.deleteFromStorage(ideasCollection);
 };
 
 
-function getIdeaById(id){
+function getIdeaById(id) {
   for(var i=0; i<ideasCollection.length; i++) {
     if(id == ideasCollection[i].id) {
-
       return ideasCollection[i];
     }
   }
